8f13b4bc0dd49e9c7b0bcad4d6c4915d78a4f7e1
Pavel_Alekseev <Pavel_Alekseev@epam.com>
2019-04-19 19:17:49 +0300

    prepared to code review and presentation

--- dashboard_checker.py
+++ dashboard_checker.py
@@ -1,9 +1,23 @@
-# TODO: add command line keys, arguments handler (fire ?)
-# TODO: dry run
 # TODO: add handler of exceptions
 # TODO: review Grafana math functions, for ex. 'asPercent'
 
+"""
+The tool goes through dashboards (panel's type 'graph') and looks for gaps in data,
+in this case, adds an annotation on a panel.
+
+Usage:
+python3 dashboard_checker.py (default run, doesn't delete old annotations, default time window and tag is 'NO DATA')
+python3 dashboard_checker.py -c 86400 (deletes old annotation for last 24h (86400 secinds),
+                                       default time window and tag is 'NO DATA')
+python3 dashboard_checker.py -d graphite (goes through only graphite datasource panels,
+                                       default time window and tag is 'NO DATA')
+python3 dashboard_checker.py -t TAG (goes through all implemented datasources panels, create annotations with tag TAG)
+python3 dashboard_checker.py -i (dry run, test mode)
+
+Combinations of arguments are available.
+"""
 import requests
+from argparse import ArgumentParser
 from elasticsearch import Elasticsearch, RequestsHttpConnection, exceptions
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
@@ -98,7 +112,7 @@ class GrafanaMaker:
         :param dash_id: id of dashboard
         :param panel_id: id of panel
         :param ref_id: letter of Grafana query
-        :param: tag: list of desired tags
+        :param: tag: list of desired tags (type: list)
         :return: just reference (json) that an annotation has been created
         """
         composed_url = '{base}/annotations'.format(base=self.url_api)
@@ -114,6 +128,15 @@ class GrafanaMaker:
         return response.text
 
     def find_annotation(self, gte, lte, dash_id, tag, limit=100):
+        """
+        Looks for annotations
+        :param gte:
+        :param lte:
+        :param dash_id:
+        :param tag:
+        :param limit:
+        :return:
+        """
         composed_url = '{base}/annotations?from={gte}&to={lte}&tags={tag}&limit={limit}&dashboardId={dash_id}'.format(
             base=self.url_api,
             gte=gte,
@@ -125,6 +148,11 @@ class GrafanaMaker:
         return response.json()
 
     def delete_annotation(self, annot_id):
+        """
+        Removes old annotation
+        :param annot_id: annotation id
+        :return:
+        """
         composed_url = '{base}/annotations/{id}'.format(
             base=self.url_api,
             id=annot_id)
@@ -235,6 +263,11 @@ class GrafanaMaker:
 
 
 def configure_logging(log_level):
+    """
+    Configures logger
+    :param log_level: logging level (INFO, DEBUG...)
+    :return: object logger
+    """
     logger = logging.getLogger(__name__)
 
     level = logging.getLevelName(log_level)
@@ -244,7 +277,7 @@ def configure_logging(log_level):
     formatter = logging.Formatter(fmt=format_str)
 
     log_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'dashboard_checker.log')
-    rotating_hndlr = RotatingFileHandler(filename=log_path, maxBytes=5*1024*1024, backupCount=2)
+    rotating_hndlr = RotatingFileHandler(filename=log_path, maxBytes=5 * 1024 * 1024, backupCount=2)
     rotating_hndlr.setFormatter(formatter)
     # workaround - avoid duplication of log records, review is required
     if logger.hasHandlers():
@@ -337,7 +370,7 @@ def panel_info(panels_info):
         if datasource is None:
             # Seems like it is a bug in Grafana
             logger.debug(
-                "Datasource for {} is None. , will be assigned as 'default'".format(panel['title']))
+                "Datasource for {} is None, will be assigned as 'default'".format(panel['title']))
             datasource = 'default'
 
         if 'Mixed' in datasource:
@@ -422,7 +455,7 @@ def elastic_request(elk_url, index_tmpl, query, time_window, proxy=''):
     :return: list of hits
     """
 
-    gte = time.strftime("%Y-%m-%dT%H:%M:%S", time.gmtime(time.time()-time_window))
+    gte = time.strftime("%Y-%m-%dT%H:%M:%S", time.gmtime(time.time() - time_window))
     lte = time.strftime("%Y-%m-%dT%H:%M:%S", time.gmtime(time.time()))
 
     # list of rules should be extended if it is necessary
@@ -430,12 +463,6 @@ def elastic_request(elk_url, index_tmpl, query, time_window, proxy=''):
         elk_url = elk_url.replace('9200', '80/elasticsearch')
     if 'elasticsearch-odh-ecx' in elk_url:
         elk_url = 'http://172.23.29.161:80/elasticsearch'
-
-    # if 'kibana-cdnrep' in elk_url:
-    #     elk_url = 'http://kibana-cdnrep:80/elasticsearch'
-    # if 'kibana-mercury' in elk_url:
-    #     elk_url = 'http://kibana-mercury:80/elasticsearch'
-
     if '80' not in elk_url:
         elk_url = 'http://{}:80/elasticsearch'.format(elk_url.split('/')[2])
 
@@ -453,36 +480,33 @@ def elastic_request(elk_url, index_tmpl, query, time_window, proxy=''):
         return None
 
 
-# TODO: add the catcher to functions
-def catch_http_error(response):
-    try:
-        response.raise_for_status()
-    except response.exceptions.HTTPError as er:
-        return "Error: {error}".format(error=er)
-    pass
-
-
-def annotation_handler(time_window, dash_id, tag='NO DATA'):
+def annotation_handler(time_window, dash_id, tag, test_mode=False):
     """
     Handles Grafana of annotations, looks for annotations on dashboard according to its id and tag and deletes it
     :param time_window: time window for search annotations, in s
     :param dash_id: id of dashboard
     :param tag: tag of annotations
+    :param test_mode: test mode
     :return:
     """
     annot_list = graf_inst.find_annotation(current_timestamp - time_window * 1000, current_timestamp, dash_id, tag)
     annotation_list_id = [annot.get('id') for annot in annot_list]
     for annot_id in annotation_list_id:
-        graf_inst.delete_annotation(annot_id)
-        logger.info("Annotation with id: {} has been deleted".format(annot_id))
+        if test_mode is False:
+            graf_inst.delete_annotation(annot_id)
+            logger.info("Annotation with id: {annot} has been deleted".format(annot=annot_id))
+        else:
+            logger.info("Annotation with id: {annot} is found on a dashboard id: {dash}".format(annot=annot_id,
+                                                                                                dash=dash_id))
 
 
-def graphite_flow(time_window, tag=['NO DATA']):
+def graphite_flow(time_window, tag, test_mode=False):
     """
     Derives graphite metric's path, gets list of metric's data for specified time window, initializes request
     creates annotations if data is null
     :param time_window: time window for search annotations, in s
-    :param tag: list of tags of annotations
+    :param tag: tag for annotations, type: string
+    :param test_mode: test mode
     :return:
     """
     extract = pretty_search(meta_dash.target_json, 'target')
@@ -496,20 +520,27 @@ def graphite_flow(time_window, tag=['NO DATA']):
             no_data += 1
 
     if no_data == len(checked_list):
-        graf_inst.create_annotation(dash_id_info.id,
-                                    meta_dash.panel_id,
-                                    ref_id(meta_dash.target_json),
-                                    tag)
-        logger.info("Annotation has been added on dashboard \"{dashboard}\" on panel \"{panel}\"".
-                    format(dashboard=dash_id_info.name, panel=meta_dash.title))
+        if test_mode is False:
+            graf_inst.create_annotation(dash_id_info.id,
+                                        meta_dash.panel_id,
+                                        ref_id(meta_dash.target_json),
+                                        [tag])
+            logger.info("Annotation has been added on dashboard \"{dashboard}\" panel \"{panel}\"".
+                        format(dashboard=dash_id_info.name,
+                               panel=meta_dash.title))
+        else:
+            logger.info("No data state is found on dashboard \"{dashboard}\" panel \"{panel}\"".
+                        format(dashboard=dash_id_info.name,
+                               panel=meta_dash.title))
 
 
-def elasticsearch_flow(time_window, tag=['NO DATA']):
+def elasticsearch_flow(time_window, tag, test_mode=False):
     """
     Derives index and formats it to template (index-*), initializes request,
     creates annotations if data is null
     :param time_window: time window for search annotations, in s
-    :param tag: list of tags of annotations
+    :param tag: tag for annotations, type: string
+    :param test_mode: test mode
     :return:
     """
     index_templ = datasource_db
@@ -521,24 +552,31 @@ def elasticsearch_flow(time_window, tag=['NO DATA']):
 
     elk_response = elastic_request(datasource_url, index_templ, query, time_window, settings.proxy)
 
+    # case when elasticsearch request returned connection error
     if elk_response is None:
         pass
 
     if not elk_response:
-        graf_inst.create_annotation(dash_id_info.id,
-                                    meta_dash.panel_id,
-                                    ref_id(meta_dash.target_json),
-                                    tag)
-        logger.debug("Elasticsearch query {} where there is no data".format(query))
-        logger.info("Annotation has been added on dashboard \"{dashboard}\" on panel \"{panel}\"".
-                    format(dashboard=dash_id_info.name, panel=meta_dash.title))
+        if test_mode is False:
+            graf_inst.create_annotation(dash_id_info.id,
+                                        meta_dash.panel_id,
+                                        ref_id(meta_dash.target_json),
+                                        [tag])
+            logger.info("Annotation has been added on dashboard \"{dashboard}\" on panel \"{panel}\"".
+                        format(dashboard=dash_id_info.name,
+                               panel=meta_dash.title))
+        else:
+            logger.info("No data state is found on dashboard \"{dashboard}\" panel \"{panel}\"".
+                        format(dashboard=dash_id_info.name,
+                               panel=meta_dash.title))
 
 
-def influxdb_flow(time_window, tag=['NO DATA']):
+def influxdb_flow(time_window, tag, test_mode=False):
     """
     Derives query, initializes request to InfluxDB and creates annotations if data is null
     :param time_window: time_window: time window for search annotations, in s
-    :param tag: list of tags of annotations
+    :param tag: tag for annotations, type: string
+    :param test_mode: test mode
     :return:
     """
     time_filter = 'time >= now() - {timewindow}m'.format(timewindow=str(int(time_window / 60)))
@@ -554,12 +592,18 @@ def influxdb_flow(time_window, tag=['NO DATA']):
             no_data += 1
 
     if no_data == len(graf_inst.influx_checker(metric_data)):
-        graf_inst.create_annotation(dash_id_info.id,
-                                    meta_dash.panel_id,
-                                    ref_id(meta_dash.target_json),
-                                    tag)
-        logger.info("Annotation has been added on dashboard \"{dashboard}\" on panel \"{panel}\"".
-                    format(dashboard=dash_id_info.name, panel=meta_dash.title))
+        if test_mode is False:
+            graf_inst.create_annotation(dash_id_info.id,
+                                        meta_dash.panel_id,
+                                        ref_id(meta_dash.target_json),
+                                        [tag])
+            logger.info("Annotation has been added on dashboard \"{dashboard}\" on panel \"{panel}\"".
+                        format(dashboard=dash_id_info.name,
+                               panel=meta_dash.title))
+        else:
+            logger.info("No data state is found on dashboard \"{dashboard}\" panel \"{panel}\"".
+                        format(dashboard=dash_id_info.name,
+                               panel=meta_dash.title))
 
 
 def relevant_time(title, skip='abs'):
@@ -608,8 +652,42 @@ Meta_dash = namedtuple('Meta_dash', ['panel_id', 'title', 'datasource', 'target_
 Id_dash = namedtuple('Id_dash', ['name', 'id', 'uid'])
 
 if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument("-d", "--check",
+                        nargs='*',
+                        dest="checker",
+                        default='all',
+                        help='list of checking sources: graphite, influxdb, elastic or all (default)')
+    parser.add_argument("-c", "--clean",
+                        dest='clean',
+                        default=0,
+                        type=int,
+                        help='time delta (in seconds), when cleaner starts to look for old annotations, '
+                             '0 as default')
+    parser.add_argument("-t", "--tag",
+                        dest='tag',
+                        default='NO DATA',
+                        type=str,
+                        help="value of a tag for search/deletion and adding annotations, 'NO DATA' as default")
+    parser.add_argument("-i", "--dry-run",
+                        dest='dry_run',
+                        action='store_true',
+                        help='use the tool without active actions, False as default')
+
+    args = parser.parse_args()
+
     logger = configure_logging(settings.log_level)
     logger.info('-------------------------------------------------')
+
+    if args.dry_run:
+        logger.debug("Test mode has been activated")
+    else:
+        logger.debug("Tool has been run with following parameters:\n"
+                     "checking datasource: {datasource}\n"
+                     "cleaner time window: {cleaner}\n"
+                     "tag: {tag}\n".format(datasource=args.checker,
+                                           cleaner=args.clean,
+                                           tag=args.tag))
 
     # create object - instance for work with Grafana
     graf_inst = GrafanaMaker(settings.url_api, settings.proxy, settings.headers)
@@ -617,8 +695,10 @@ if __name__ == '__main__':
     id_info_list = []
     for dash in settings.dash_list:
         try:
-            id_info_list.append(Id_dash(dash, graf_inst.get_uid(dash)[0], graf_inst.get_uid(dash)[1]))
-        except TypeError:
+            dash_id, dash_uid = graf_inst.get_uid(dash)
+            id_info_list.append(Id_dash(dash, dash_id, dash_uid))
+        except TypeError as err:
+            logger.debug("{dash}: {err}".format(dash=dash, err=err))
             continue
 
     for dash_id_info in id_info_list:
@@ -627,12 +707,10 @@ if __name__ == '__main__':
         # current time in ms
         current_timestamp = int(time.time() * 1000)
 
-        annotation_handler(TIMEPREFIX.get('d'), dash_id_info.id)
+        annotation_handler(args.clean, dash_id_info.id, args.tag, args.dry_run)
 
         # main flow: check data and set annotations
         for meta_dash in panel_info(graf_inst.get_panels_info(dash_id_info.uid)):
-            logger.info("Panel \"{panel}\" (query \"{query}\") is checking...".
-                        format(panel=meta_dash.title, query=ref_id(meta_dash.target_json)))
 
             timewindow = relevant_time(meta_dash.title)
             if timewindow is None:
@@ -649,11 +727,17 @@ if __name__ == '__main__':
             datasource_url = datasource_info['url']
             datasource_db = datasource_info['database']
 
-            if 'graphite' in datasource_info['type']:
-                graphite_flow(timewindow)
+            if 'graphite' in datasource_info['type'] and ('graphite' in args.checker or 'all' in args.checker):
+                logger.info("Panel \"{panel}\" (query \"{query}\") is checking...".
+                            format(panel=meta_dash.title, query=ref_id(meta_dash.target_json)))
+                graphite_flow(timewindow, args.tag, args.dry_run)
 
-            if 'elasticsearch' in datasource_info['type']:
-                elasticsearch_flow(timewindow)
+            if 'elasticsearch' in datasource_info['type'] and ('elastic' in args.checker or 'all' in args.checker):
+                logger.info("Panel \"{panel}\" (query \"{query}\") is checking...".
+                            format(panel=meta_dash.title, query=ref_id(meta_dash.target_json)))
+                elasticsearch_flow(timewindow, args.tag, args.dry_run)
 
-            if 'influxdb' in datasource_info['type']:
-                influxdb_flow(timewindow)
+            if 'influxdb' in datasource_info['type'] and ('influxdb' in args.checker or 'all' in args.checker):
+                logger.info("Panel \"{panel}\" (query \"{query}\") is checking...".
+                            format(panel=meta_dash.title, query=ref_id(meta_dash.target_json)))
+                influxdb_flow(timewindow, args.tag, args.dry_run)
